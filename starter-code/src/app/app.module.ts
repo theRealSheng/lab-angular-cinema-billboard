d8c007c2a7e4d9eb2cccec47edd16b5cca2b0007
theRealSheng <sheng.huchai@gmail.com>
2018-02-21 19:10:11 +0100

    Add service movies

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MoviesService } from 'app/services/movies.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [MoviesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
